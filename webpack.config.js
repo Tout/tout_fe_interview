7c0d27f4b929188112a537486b4415e9fb74f4bf
Chris Richards <ripter001@gmail.com>
2017-06-16 10:45:07 -0700

    pulled out the css webpack rules.

--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,6 @@ const baseConfig = {
         loader: 'babel-loader',
         include: [
           path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules/tout_video_player/src'),
-          path.resolve(__dirname, 'node_modules/tout_analytics_client'),
-        ],
-      },
-      // Compile CSS with modules
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
         ],
       },
     ], // rules:
